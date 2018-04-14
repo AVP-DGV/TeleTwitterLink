57036a723b0ffd2733c9702530479e9522254a09
Dobri V <dobromir_varbanov@abv.bg>
2018-04-14 15:34:11 +0300

    DbContextNotWorking

---
+++ TeleTwitterLink/TeleTwitterLInk.Data/TeleTwitterLinkDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using TeleTwitterLink.Data.Models;
+
+namespace TeleTwitterLInk.Data
+{
+    public class TeleTwitterLinkDbContext : IdentityDbContext<User>
+    {
+        public TeleTwitterLinkDbContext(DbContextOptions<TeleTwitterLinkDbContext> options)
+           : base(options)
+        {
+        }
+    }
+}
