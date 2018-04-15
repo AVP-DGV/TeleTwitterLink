fa426c8df736bdc890522941d9ae7f26eec3b07a
AlexPetrova <a.petrova95@yahoo.com>
2018-04-15 14:07:04 +0300

    added TeleTwitterLinkDbContextFactory

---
+++ TeleTwitterLink/TeleTwitterLInk.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using TeleTwitterLInk.Data;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<TeleTwitterLinkDbContext>
+{
+    public TeleTwitterLinkDbContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(@"E:\Програмиране\Telerik Academy Alpha\Mod4\TeleTwitterLink\TeleTwitterLink\TeleTwitterLink.Web\appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<TeleTwitterLinkDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        builder.UseSqlServer(connectionString);
+        return new TeleTwitterLinkDbContext(builder.Options);
+    }
+}
