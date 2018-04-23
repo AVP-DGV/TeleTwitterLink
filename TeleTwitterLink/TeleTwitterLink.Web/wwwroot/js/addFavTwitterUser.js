d9da9e68a060f3834ed97e9e0b05b1b3fb3e9cdb
AlexPetrova <a.petrova95@yahoo.com>
2018-04-23 10:27:08 +0300

    button add working a little bit more :D

--- TeleTwitterLink/TeleTwitterLink.Web/wwwroot/js/addFavTwitterUser.js
+++ TeleTwitterLink/TeleTwitterLink.Web/wwwroot/js/addFavTwitterUser.js
@@ -1,6 +1,9 @@
 ﻿$(function () {
     $('#search-results').on('click', 'ul li button', function (event) {
-        var id = $('.add-fav-user').attr('id');
-        $('#' + id).text('Added');
+        event.preventDefault();
+        var id = $(this).attr('id');
+
+        var button = $('#' + id).text('Added').prop('disabled', true);
+        
     });
 });
