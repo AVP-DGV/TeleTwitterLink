4e8a6c316a53a6f2272d6dfb6f80a519767cd818
Dobri V <dobromir_varbanov@abv.bg>
2018-04-23 18:51:19 +0300

    ConflictsInAddButton

--- TeleTwitterLink/TeleTwitterLink.Web/wwwroot/js/ShowMore.js
+++ TeleTwitterLink/TeleTwitterLink.Web/wwwroot/js/ShowMore.js
@@ -1,4 +1,7 @@
 ﻿$(function () {
+    //$('#search-results').on('click', 'ul li button', function (event) {
+    //    $('.additional-info').css('overflow', 'visible');
+
     $(document).ready(function () {
         $('[data-toggle="popover"]').popover();
     });
