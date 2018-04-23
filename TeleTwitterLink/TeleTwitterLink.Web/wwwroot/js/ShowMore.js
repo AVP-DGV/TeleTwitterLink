d0f3a89aa019d5f1947bdd1f573aaa4f51524c54
AlexPetrova <a.petrova95@yahoo.com>
2018-04-23 14:00:42 +0300

    css for the search results

--- TeleTwitterLink/TeleTwitterLink.Web/wwwroot/js/ShowMore.js
+++ TeleTwitterLink/TeleTwitterLink.Web/wwwroot/js/ShowMore.js
@@ -1,5 +1,5 @@
 ﻿$(function () {
-    $('#search-results').on('click', 'ul li button', function (event) {
-        $('.additional-info').css('overflow', 'visible');
+    $(document).ready(function () {
+        $('[data-toggle="popover"]').popover();
     });
 });
