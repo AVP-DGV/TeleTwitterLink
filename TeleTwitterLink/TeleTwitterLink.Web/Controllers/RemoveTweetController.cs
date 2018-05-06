defbd18a1e3c6663d60d9d67c53803977d143f65
AlexPetrova <a.petrova95@yahoo.com>
2018-05-06 21:18:34 +0300

    remove tweet

---
+++ TeleTwitterLink/TeleTwitterLink.Web/Controllers/RemoveTweetController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TeleTwitterLink.Data.Models;
+using TeleTwitterLink.DTO;
+using TeleTwitterLink.Services.Data.Contracts;
+
+namespace TeleTwitterLink.Web.Controllers
+{
+    public class RemoveTweetController : Controller
+    {
+        private ITweetService tweetService;
+        private ITwitterApiService twitterApiService;
+        private UserManager<User> userManager;
+
+        public RemoveTweetController(ITweetService tweetService, ITwitterApiService twitterApiService, UserManager<User> userManager)
+        {
+            this.tweetService = tweetService;
+            this.twitterApiService = twitterApiService;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult RemoveTweet(TweetDTO tweetDTO)
+        {
+            //validate if the tweet id is correct
+            var aspUserId = this.userManager.GetUserId(HttpContext.User);
+
+            this.tweetService.RemoveTweet(tweetDTO.TweetId, aspUserId);
+
+            return Ok();
+        }
+    }
+}
---
+++ TeleTwitterLink/TeleTwitterLink.Web/wwwroot/js/removeTweet.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    $('#list-favourite-tweets').on('click', 'div div button',
+        function (event) {
+            event.preventDefault();
+            var data = $(this).closest('form').serialize();
+            var id = $(this).attr('id');
+
+            $.post(
+                '/RemoveTweet/RemoveTweet',
+                data,
+                function (response) {
+                    var button = $('#' + id).text('Removed').prop('disabled', true);
+                }
+            )
+        }
+    )
+});
