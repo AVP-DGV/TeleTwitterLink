d52e45d94bd997cc93edf0e03be51dc75d417dfa
Dobri V <dobromir_varbanov@abv.bg>
2018-05-07 23:50:43 +0300

    TestProbes

---
+++ TeleTwitterLink/TeleTwitterLink.Tests/ServiceTests/DeserializerOfJsonTests/DeserializerOfJson_Should.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TeleTwitterLink.Services.Data;
+
+namespace TeleTwitterLink.Tests.ServiceTests.DeserializerOfJsonTests
+{
+    [TestClass]
+    public class DeserializerOfJson_Should
+    {
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenStringIsEmpty()
+        {
+            var test = new DeserializerOfJson();
+            Assert.ThrowsException<ArgumentException>(() => test.DeserializeJson<string>(string.Empty));
+        }
+
+        [TestMethod]
+        public void ReturnConvertedJsons()
+        {
+            //var test = new DeserializerOfJson();
+            //string json = "{1, 2, 3}";
+            //var int[] ={[JsonProperty("id_str")]};
+            
+            //var result = new[] { 1, 2, 3 };
+            //T convertedJsons = JsonConvert.DeserializeObject<T>(json);
+            //Assert.AreEqual(result, test.DeserializeJson<int[]>(json));
+        }
+    }
+}
